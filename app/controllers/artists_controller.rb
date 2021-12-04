11ff471e150cd1b3b1958d77d158e606e5506dce
Hannah Warren <hannahkwarren@gmail.com>
2021-12-04 13:48:19 -0500

    Add edit and update functionality for artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,6 +12,18 @@ class ArtistsController < ApplicationController
     redirect_to "/artists"
   end
 
+  def edit
+    @artist = Artist.find(params[:id])
+  end
+
+  def update
+    artist = Artist.find(params[:id])
+    artist.update(artist_params)
+    redirect_to "/artists"
+  end
+
+private #only available inside classes they're defined in (this controller)
+#for security and good encapsulation
   def artist_params
     params.permit(:name)
   end
